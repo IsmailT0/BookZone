fb305104bd88e339eda0231af266c838fe6378fe
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-11-19 21:30:28 +0300

    update product

--- BookZone.Model/obj/Debug/net8.0/BookZone.Models.AssemblyInfo.cs
+++ BookZone.Model/obj/Debug/net8.0/BookZone.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookZone.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345d281b0ed6e40012ddd00ca6ca91d49535eb31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bdddb15b8722acfabbafba947405165ccda4433")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookZone.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookZone.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
