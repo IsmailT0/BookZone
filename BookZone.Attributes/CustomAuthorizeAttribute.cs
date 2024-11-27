caedf3ecc80fcb8cc47341c71600036ed6fbe890
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-11-27 23:26:32 +0300

    add custom authorization

---
+++ BookZone.Attributes/CustomAuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+
+namespace BookZone.Attributes
+{
+    public class CustomAuthorizeAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var userId = context.HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary
+                {
+                    { "controller", "Auth" },
+                    { "action", "Login" },
+                    { "area", "Auth" }
+                });
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
