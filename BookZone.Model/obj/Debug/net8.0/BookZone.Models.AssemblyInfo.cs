4d2d327c18718bbdf8967eb0edc9f4f1bd4420b1
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-11-28 21:39:29 +0300

    add shopping cart view model

--- BookZone.Model/obj/Debug/net8.0/BookZone.Models.AssemblyInfo.cs
+++ BookZone.Model/obj/Debug/net8.0/BookZone.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookZone.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b40428adbdb0a343cb36db2173a7cfd202a9f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb77ac664766efb4b94da1a8aa115c1a08416fce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookZone.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookZone.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BookZone/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,44 @@
+﻿using BookZone.Attributes;
+using BookZone.DataAccess.Repository.IRepository;
+using BookZone.Models;
+using BookZone.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookZone.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [CustomAuthorize]
+    public class ShoppingCartController : Controller
+    {
+        public readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM shoppinCartVM { get; set; }
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ShoppingCartVM shoppingCartVM;
+            if (int.TryParse(userId, out int parsedUserId))
+            {
+                shoppingCartVM = new ShoppingCartVM
+                {
+                    ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == parsedUserId, includeProperties: "Product"),
+                    OrderTotal = 0
+                };
+            }
+            else
+            {
+                // Handle the case where userId is not a valid int
+                return BadRequest("Invalid user ID");
+            }
+
+
+            return View(shoppingCartVM);
+        }
+    }
+}
